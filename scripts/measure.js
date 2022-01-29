a87a050bb64fcec0be1780aa949dfd184dcfc10e
Fynn Becker <post@fynn.be>
2022-01-29 08:42:20 +0100

    feat: Measure paint time

--- scripts/measure.js
+++ scripts/measure.js
@@ -17,76 +17,88 @@ const images = await fs.readdir(IMAGES_DIR)
 await fs.mkdir(TRACES_DIR, { recursive: true })
 await fs.mkdir(RESULTS_DIR, { recursive: true })
 
-server.listen(PORT)
-
-const browser = await puppeteer.launch()
-const page = await browser.newPage()
-const client = await page.target().createCDPSession()
-await client.send('Network.emulateNetworkConditions', NETWORK_CONDITIONS)
-await page.setViewport({ width: 1200, height: 1200 })
-await page.goto(`http://localhost:${PORT}/`)
-
 const resultPath = `${RESULTS_DIR}/${new Date().toISOString()}.csv`
 await fs.writeFile(resultPath, valuesToCSV(
   'Name',
   'Type',
   'Size',
-  'Size (kB)',
   'Response Time',
-  'Response Time (ms)',
   'Decode Time',
-  'Decode Time (ms)'
+  'Paint Time'
 ))
 
-async function measure () {
-  for (const image of images) {
-    const imagePath = `${IMAGES_DIR}/${image}`
-    const tracePath = `${TRACES_DIR}/${image}.json`
+server.listen(PORT)
 
-    await fs.writeFile(tracePath, '')
+const browser = await puppeteer.launch()
+const page = await browser.newPage()
+const client = await page.target().createCDPSession()
 
-    await page.reload()
-    await page.tracing.start({ path: tracePath })
+await client.send('Network.emulateNetworkConditions', NETWORK_CONDITIONS)
 
-    await page.evaluate((url) => {
-      return new Promise((resolve) => {
-        const img = new Image()
-        img.src = url
-        img.decode().then(() => {
-          document.body.appendChild(img)
-          resolve()
-        })
-      })
-    }, imagePath)
+await page.setViewport({ width: 1200, height: 1200 })
+await page.goto(`http://localhost:${PORT}/`)
 
-    await page.tracing.stop()
+/**
+ * Load image in browser context
+ *
+ * @param {string} url Image URL
+ * @returns {Promise<void>}
+ */
+function loadImageInBrowser (url) {
+  return new Promise((resolve) => {
+    const img = new Image()
+    img.src = url
+    img.decode().then(() => {
+      document.body.appendChild(img)
 
-    const traceFile = await fs.readFile(tracePath, { encoding: 'utf8' })
-    const { traceEvents } = JSON.parse(traceFile)
+      // Paint finishes after two frames.
+      requestAnimationFrame(() => requestAnimationFrame(() => resolve()))
+    })
+  })
+}
 
-    const fileExtension = path.extname(image)
-    const fileName = path.basename(image, fileExtension)
-    const fileStats = await fs.stat(imagePath)
-    const resourceStart = traceEvents.find(trace => trace.name === 'ResourceSendRequest').ts
-    const resourceFinish = traceEvents.find(trace => trace.name === 'ResourceFinish').ts
-    const responseDuration = resourceFinish - resourceStart
-    const decodeDuration = traceEvents.find(trace => trace.name === 'ImageDecodeTask').dur
+/**
+ * Measure image performance
+ *
+ * @param {string} image Image name
+ * @returns {Promise<void>}
+ */
+async function measure (image) {
+  const imagePath = `${IMAGES_DIR}/${image}`
+  const tracePath = `${TRACES_DIR}/${image}.json`
+  await fs.writeFile(tracePath, '')
 
-    await fs.appendFile(resultPath, valuesToCSV(
-      fileName,
-      fileExtension,
-      fileStats.size,
-      fileStats.size / 1000,
-      responseDuration,
-      responseDuration / 1000,
-      decodeDuration,
-      decodeDuration / 1000
-    ))
-  }
+  await page.reload()
+  await page.tracing.start({ path: tracePath })
+  await page.evaluate(loadImageInBrowser, imagePath)
+  await page.tracing.stop()
+
+  const fileExtension = path.extname(image)
+  const fileName = path.basename(image, fileExtension)
+  const { size: fileSize } = await fs.stat(imagePath)
+
+  const traceFile = await fs.readFile(tracePath, { encoding: 'utf8' })
+  const { traceEvents } = JSON.parse(traceFile)
+
+  const resourceStart = traceEvents.find(trace => trace.name === 'ResourceSendRequest').ts
+  const resourceFinish = traceEvents.find(trace => trace.name === 'ResourceFinish').ts
+  const decodeDuration = traceEvents.find(trace => trace.name === 'ImageDecodeTask').dur
+  const paintDuration = traceEvents.find(trace => trace.name === 'PaintImage').dur
+
+  await fs.appendFile(resultPath, valuesToCSV(
+    fileName,
+    fileExtension,
+    fileSize,
+    resourceFinish - resourceStart,
+    decodeDuration,
+    paintDuration
+  ))
 }
 
 for (let index = 0; index < RERUNS; index++) {
-  await measure()
+  for (const image of images) {
+    await measure(image)
+  }
 }
 
 await browser.close()
